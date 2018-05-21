c12c88453a280d98b503dd72f2aae3a6924686a2
Sebastian Krein <sebastian@itstrategen.de>
2018-05-21 10:33:41 +0200

    Load minified versions on live scenario

--- app/common/config/main.php
+++ app/common/config/main.php
@@ -6,10 +6,30 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'cache'      => [
+        'cache'        => [
             'class' => 'yii\caching\FileCache',
         ],
-        'urlManager' => [
+        'assetManager' => [
+            'class'   => 'yii\web\AssetManager',
+            'bundles' => [
+                'yii\web\JqueryAsset'                => [
+                    'js' => [
+                        YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+                    ]
+                ],
+                'yii\bootstrap\BootstrapAsset'       => [
+                    'css' => [
+                        YII_ENV_DEV ? 'css/bootstrap.css' : 'css/bootstrap.min.css',
+                    ]
+                ],
+                'yii\bootstrap\BootstrapPluginAsset' => [
+                    'js' => [
+                        YII_ENV_DEV ? 'js/bootstrap.js' : 'js/bootstrap.min.js',
+                    ]
+                ]
+            ],
+        ],
+        'urlManager'   => [
             'enablePrettyUrl' => true,
             'showScriptName'  => false,
         ],
