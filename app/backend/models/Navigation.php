5070780c4ec8cbee2d1f91a171b76f2aff9320fa
Sebastian Krein <sebastian@itstrategen.de>
2018-07-15 10:05:08 +0200

    Update position if parent has changed

--- app/backend/models/Navigation.php
+++ app/backend/models/Navigation.php
@@ -13,5 +13,29 @@ namespace backend\models;
  */
 class Navigation extends \common\models\Navigation
 {
+    /**
+     * Before saving the record update the value of position dependent on whether the parent record has changed, to the
+     * highest position available within the new Navigation record set automatically.
+     *
+     * @param bool $insert
+     *
+     * @return bool
+     */
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
 
+            /*
+             * If parent has changed determine the next highest position.
+             */
+            if (is_null($this->position) || $this->isAttributeChanged('parent_id')) {
+
+                $this->position = $this::find()->where(['parent_id' => $this->parent_id])->max('position') + 1;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
 }
