052ca7d30f3a1d81c16b3a36df305de5d129ab85
Sebastian Krein <sebastian@itstrategen.de>
2018-11-09 17:52:49 +0100

    Added Growl widget and show them as flash messages within layout

--- app/backend/views/layouts/sb-admin-2.php
+++ app/backend/views/layouts/sb-admin-2.php
@@ -29,6 +29,24 @@ SbAdmin2Asset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
+<?php foreach (Yii::$app->session->getAllFlashes() as $message):; ?>
+    <?= \kartik\growl\Growl::widget([
+        'type' => (!empty($message['type'])) ? $message['type'] : 'danger',
+        'title' => (!empty($message['title'])) ? Html::encode($message['title']) : 'Title Not Set!',
+        'icon' => (!empty($message['icon'])) ? $message['icon'] : 'fa fa-info',
+        'body' => (!empty($message['message'])) ? Html::encode($message['message']) : 'Message Not Set!',
+        'showSeparator' => true,
+        'delay' => 1, //This delay is how long before the message shows
+        'pluginOptions' => [
+            'delay' => (!empty($message['duration'])) ? $message['duration'] : 3000, //This delay is how long the message shows for
+            'placement' => [
+                'from' => (!empty($message['positonY'])) ? $message['positonY'] : 'top',
+                'align' => (!empty($message['positonX'])) ? $message['positonX'] : 'right',
+            ]
+        ]
+    ]); ?>
+<?php endforeach; ?>
+
 <div id="wrapper">
     <?php
     NavBar::begin([
