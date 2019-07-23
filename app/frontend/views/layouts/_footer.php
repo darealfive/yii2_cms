b9c0349f664269ce243284c893361b1573dd63de
Sebastian Krein <sebastian@itstrategen.de>
2019-07-23 08:49:53 +0200

    Style vertical divider of menu. Removed copyright

--- app/frontend/views/layouts/_footer.php
+++ app/frontend/views/layouts/_footer.php
@@ -1,25 +1,21 @@
 <?php
 
-use yii\helpers\Html;
-
 ?>
 <hr class="mt-5">
 
-<ul class="nav justify-content-center">
-    <li class="nav-item">
-        <a class="nav-link active" href="#">facebook</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">modelkartei</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">500px</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">instagram</a>
-    </li>
-</ul>
-
-<footer class="footer">
-    <p class="text-right">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+<footer class="footer mb-3">
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">facebook</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">modelkartei</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">500px</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">instagram</a>
+        </li>
+    </ul>
 </footer>
