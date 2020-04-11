dce6778d8894811435f0342a2cd076e3c194acbc
Sebastian Krein <sebastian@itstrategen.de>
2020-04-11 11:10:14 +0200

    Adjust path because of dockerisation

--- app/frontend/views/gallery/index.php
+++ app/frontend/views/gallery/index.php
@@ -14,13 +14,13 @@ $this->title = 'My Yii Application';
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="//frontend.test:8888/images/gallery/980x380/001.jpg" class="d-block w-100" alt="...">
+                <img src="//localhost:8100/images/gallery/980x380/001.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="//frontend.test:8888/images/gallery/980x380/002.jpg" class="d-block w-100" alt="...">
+                <img src="//localhost:8100/images/gallery/980x380/002.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="//frontend.test:8888/images/gallery/980x380/003.jpg" class="d-block w-100" alt="...">
+                <img src="//localhost:8100/images/gallery/980x380/003.jpg" class="d-block w-100" alt="...">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
--- app/frontend/views/site/index.php
+++ app/frontend/views/site/index.php
@@ -9,13 +9,13 @@ $this->title = 'My Yii Application';
     <div class="carousel slide" data-ride="carousel" data-interval="3000">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="//frontend.test:8888/images/gallery/980x380/001.jpg" class="d-block w-100" alt="...">
+                <img src="//localhost:8100/images/gallery/980x380/001.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="//frontend.test:8888/images/gallery/980x380/002.jpg" class="d-block w-100" alt="...">
+                <img src="//localhost:8100/images/gallery/980x380/002.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="//frontend.test:8888/images/gallery/980x380/003.jpg" class="d-block w-100" alt="...">
+                <img src="//localhost:8100/images/gallery/980x380/003.jpg" class="d-block w-100" alt="...">
             </div>
         </div>
     </div>
